0e915ec6597970907e8531ae3ebc550c7c67d4af
BadeaFreh <71319309+BadeaFreh@users.noreply.github.com>
2023-02-14 10:56:24 +0200

    solved

--- apt-filter.js
+++ apt-filter.js
@@ -1,8 +1,7 @@
-//Only start playing with this file if you're done with all the basics
 
 const must = (radio) => radio.filter(rb => rb.checked).length
 
-const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate) {
+const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate, isParking) {
     let relevantApts = apartments.filter(a =>
         a.address.toLowerCase().includes(address.toLowerCase() || "") &&
         a.price >= (minPrice || 0) &&
@@ -12,5 +11,7 @@ const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRoo
     )
 
     immediate = !immediate[0].checked
-    return immediate ? relevantApts : relevantApts.filter(a => (a.immediate)) 
+    isParking = !isParking[0].checked
+    relevantApts = immediate ? relevantApts : relevantApts.filter(a => (a.immediate))
+    return isParking ? relevantApts : relevantApts.filter(a => (a.parking))
 }
--- main.js
+++ main.js
@@ -6,15 +6,18 @@ $("button").on("click", function () {
     let minRooms = $("#min-r-input").val()
     let maxRooms = $("#max-r-input").val()
     let immediate = $("#immed-y")
-
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    let isParking = $("#park-y")
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate, isParking)
     renderApts(relevantApts)
 })
 
 const renderApts = function (apartments) {
     $("#results").empty()
     console.log(apartments) //array of apartments to render
-    //Your code goes here.
+    const source = $('#template').html()
+    const template = Handlebars.compile(source)
+    const newHTML = template({apartments})
+    $('#results').append(newHTML)
 }
 
 renderApts(apartments) //renders apartments when page loads
